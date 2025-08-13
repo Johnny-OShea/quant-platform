f3b4afbaddd437a05ed119ffdb807ea72e442c66
Johnny <brawlingbunny123@gmail.com>
2025-08-13 10:04:25 -0400

    [Setup] Created sign-in skeleton Allows individuals to work on the sign in module asynchronously

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import Home from "./screens/Home";
 import Register from "./screens/Register";
+import SignIn from "./screens/SignIn";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route element={<AppLayout />}>
                     <Route path="/" element={<Home />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/signin" element={<SignIn></SignIn>} />
                 </Route>
             </Routes>
         </Router>
